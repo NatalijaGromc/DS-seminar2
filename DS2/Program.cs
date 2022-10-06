be85c1c53909c7b88d8377f1b6d0e4fa73ef0e4d
natasha <gromc-natalija@rambler.ru>
2022-10-06 18:15:10 +0300

    дз2

---
+++ DS2/Program.cs
@@ -0,0 +1,20 @@
+﻿int N;
+string userEnter;
+
+Console.Write ("Введите число N: ");
+userEnter = Console.ReadLine()!;
+N = int.Parse(userEnter);
+
+if (100<=N)
+
+{
+while (N>=1000)
+{
+N=N/10;
+}
+Console.Write($"{N/1000*10+N%10}");
+}
+else
+{
+Console.Write ($"Третья цифра отсутствует");
+}
