9108c991573bcca5d11fb5fe69ae9fbdf31e6089
natasha <gromc-natalija@rambler.ru>
2022-10-06 18:26:56 +0300

    дз3

---
+++ DS3/Program.cs
@@ -0,0 +1,16 @@
+﻿int N;
+string userEnter;
+
+Console.Write ("Введите число N: ");
+userEnter = Console.ReadLine()!;
+N = int.Parse(userEnter);
+
+if ((6<=N)&&(N<=8))
+{
+    Console.Write ($"Выходной");
+}
+
+if ((1<=N)&&(N<6))
+{
+Console.Write($"Будний день");
+}
